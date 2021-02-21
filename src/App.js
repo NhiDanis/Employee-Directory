728c160a7a6ad41dc584f09f4af351d82b41fb34
Nhi Danis <a.nhi1001@gmail.com>
2021-02-20 20:41:54 -0800

    commit search bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import Jumbotron from './components/jumbotron';
+import SearchBar from "./components/searchBar";
 
 function App() {
   return (
     <div className="App">
       <Jumbotron/>
+      <SearchBar/>
       
     </div>
   );
