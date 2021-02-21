e112b6a631f6ec388a1f5f9a5446f0631d61d520
Nhi Danis <a.nhi1001@gmail.com>
2021-02-20 21:05:04 -0800

    commit table & install others dependencies

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Jumbotron from './components/jumbotron';
 import SearchBar from "./components/searchBar";
+import Table from "./components/table";
 
 function App() {
   return (
     <div className="App">
       <Jumbotron/>
-      <SearchBar/>
+      <Table/>
       
     </div>
   );
